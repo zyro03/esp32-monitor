085d3a309508855239a4d5cd08a8757285c7db0b
Bartosz <bar.you3@gmail.com>
2026-06-26 09:59:12 +0200

    test serial monitor

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,23 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+int licznik = 0;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  delay(1000);
+
+  Serial.println("Start programu");
+  Serial.println("Test serial monitora");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  licznik++;
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  Serial.print("Komunikat nr.: ");
+  Serial.println(licznik);
+
+  Serial.print("Czas: ");
+  Serial.print(millis() / 1000);
+
+  delay(1000);
 }
