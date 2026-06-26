a6e8fc0d90eb6c59186a7214a08105765189eff5
Bartosz <bar.you3@gmail.com>
2026-06-26 10:47:53 +0200

    dht22 readings on lcd

--- src/main.cpp
+++ src/main.cpp
@@ -26,9 +26,43 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);
 int counter = 0;
 
 void setup(){
-
-}
+  Serial.begin(115200);
+  delay(1000);
+  Wire.begin(21, 22);
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
+  dht.begin();}
 
 void loop(){
+  counter++;
+  float temperature = dht.readTemperature();
+  float humidity = dht.readHumidity();
+  lcd.clear();
   
+  if (isnan(temperature) || isnan(humidity)){
+    Serial.println("DHT22 ERROR");
+    lcd.setCursor(0,0);
+    lcd.print("DHT22 ERROR");
+  }
+  else{
+    Serial.print("Temperature: ");
+    Serial.print(temperature);
+    Serial.println(" C");
+
+    Serial.print("Humidity: ");
+    Serial.print(humidity);
+    Serial.println(" %");
+
+    lcd.setCursor(0, 0);
+    lcd.print("Temp: ");
+    lcd.print(temperature, 1);
+    lcd.print(" C");
+
+    lcd.setCursor(0, 1);
+    lcd.print("Humi: ");
+    lcd.print(humidity, 1);
+    lcd.print(" %");
+  }
+  delay(10000);
 }
