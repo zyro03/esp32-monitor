400c2cd92507c85dca554ca938e9072af4ffab38
Bartosz <bar.you3@gmail.com>
2026-06-26 10:34:29 +0200

    dht and lcd setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,34 @@
+/*
+ * LCD:
+ * VCC -> 5V
+ * GND -> GND
+ * SDA -> GPIO 21
+ * SCL -> GPIO 22
+ *
+ * DHT22:
+ * VCC  -> 3V3
+ * GND  -> GND
+ * DATA -> GPIO 4
+ */
+
 #include <Arduino.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include <DHT.h>
 
-int licznik = 0;
+#define DHT_PIN 4
+#define DHT_TYPE DHT22
 
-void setup() {
-  Serial.begin(115200);
-  delay(1000);
+DHT dht(DHT_PIN, DHT_TYPE);
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-  Serial.println("Start programu");
-  Serial.println("Test serial monitora");
+
+int counter = 0;
+
+void setup(){
+
 }
 
-void loop() {
-  licznik++;
-
-  Serial.print("Komunikat nr.: ");
-  Serial.println(licznik);
-
-  Serial.print("Czas: ");
-  Serial.print(millis() / 1000);
-
-  delay(1000);
+void loop(){
+  
 }
