a5ff0e2836e365b26d3425426427605bb3fd3a7a
Bartosz <bar.you3@gmail.com>
2026-06-29 11:16:30 +0200

    config

---
+++ docs/wiring.mg
@@ -0,0 +1,21 @@
+/*
+ * LCD:
+ * VCC -> 5V
+ * GND -> GND
+ * SDA -> GPIO 21
+ * SCL -> GPIO 22
+ *
+ * DHT22:
+ * VCC  -> 3V3
+ * GND  -> GND
+ * DATA -> GPIO 4
+ *
+ * Buzzer:
+ * VCC -> 3V3
+ * GND -> GND
+ * I/O -> GPIO 16
+ *
+ * LED:
+ * + -> GPIO 18 - 220ohm
+ * - -> GND
+ */
--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,9 @@
-/*
- * LCD:
- * VCC -> 5V
- * GND -> GND
- * SDA -> GPIO 21
- * SCL -> GPIO 22
- *
- * DHT22:
- * VCC  -> 3V3
- * GND  -> GND
- * DATA -> GPIO 4
- *
- * Buzzer:
- * VCC -> 3V3
- * GND -> GND
- * I/O -> GPIO 16
- *
- * LED:
- * + -> GPIO 18 - 220ohm
- * - -> GND
- */
-
+#include "secrets.h"
 #include <Arduino.h>
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 #include <DHT.h>
 #include <WiFi.h>
-#include "secrets.h"
 #include <PubSubClient.h>
 #include <ArduinoJson.h>
 
